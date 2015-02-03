1a83dbd338130d8b4fc40fc4841cd010a5ad394c
Scott Adie <scottynosabe@gmail.com>
2015-02-03 11:16:39 -0500

    fix k

--- k.py
+++ k.py
@@ -3,16 +3,6 @@ import redis
 
 db = redis.Redis(db=3)
 
-@willie.module.commands('kadd')
-def mapimages(bot, trigger):
-  img = trigger.group(2)
-
-  if img:
-    db.set(img, None)
-    bot.say("Added.")
-  else:
-    bot.say("-- .k <url>")
-
 @willie.module.commands('k')
 def rand_image(bot, trigger):
   img = trigger.group(2)
