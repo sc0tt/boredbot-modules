4f41911065dd323fce9c46643f30597974440c57
Scott Adie <scottynosabe@gmail.com>
2015-08-14 08:53:15 -0400

    Put things in variables.

--- twitch.py
+++ twitch.py
@@ -18,14 +18,27 @@ def monitor_streamers(bot):
   streaming = requests.get('https://api.twitch.tv/kraken/streams', params={"channel": ",".join(streamers)}).json()
   results = []
   for streamer in streaming["streams"]:
-    if streamer["channel"]["name"] not in currently_streaming:
-      currently_streaming[streamer["channel"]["name"]] = streamer["channel"]["game"]
-      results.append("%s just went live playing %s! (%s, %s viewer%s)" % (streamer["channel"]["name"], streamer["channel"]["game"], streamer["channel"]["url"], streamer["viewers"], "s" if streamer["viewers"] != 1 else ""))
-    elif streamer["channel"]["game"] != currently_streaming[streamer["channel"]["name"]]:
-      currently_streaming[streamer["channel"]["name"]] = streamer["channel"]["game"]
-      results.append("%s just started playing %s! (%s, %s viewer%s)" % (streamer["channel"]["name"], streamer["channel"]["game"], streamer["channel"]["url"], streamer["viewers"], "s" if streamer["viewers"] != 1 else ""))
+    streamer_name = streamer["channel"]["name"]
+    streamer_game = streamer["channel"]["game"]
+    streamer_url = streamer["channel"]["url"]
+    streamer_viewers = streamer["viewers"]
+    
+    if streamer_name not in currently_streaming:
+      currently_streaming[streamer_name] = streamer_game
+      results.append("%s just went live playing %s! (%s, %s viewer%s)" % (streamer_name, 
+                                                                          streamer_game, 
+                                                                          streamer_url, 
+                                                                          streamer_viewers, 
+                                                                          "s" if streamer_viewers != 1 else ""))
+    elif streamer_game != currently_streaming[streamer_name]:
+      currently_streaming[streamer_name] = streamer_game
+      results.append("%s just started playing %s! (%s, %s viewer%s)" % (streamer_name, 
+                                                                        streamer_game, 
+                                                                        streamer_url, 
+                                                                        streamer_viewers, 
+                                                                        "s" if streamer_viewers != 1 else ""))
 
-    streaming_names.append(streamer["channel"]["name"])
+    streaming_names.append(streamer_name)
 
   if results:
     bot.msg(announce_chan, ", ".join(results))  
@@ -46,7 +59,16 @@ def streamer_status(bot, trigger):
   streaming = requests.get('https://api.twitch.tv/kraken/streams', params={"channel": ",".join(query)}).json()
   results = []
   for streamer in streaming["streams"]:
-    results.append("%s is playing %s (%s, %s viewer%s)" % (streamer["channel"]["name"], streamer["channel"]["game"], streamer["channel"]["url"], streamer["viewers"], "s" if streamer["viewers"] != 1 else "" ))
+    streamer_name = streamer["channel"]["name"]
+    streamer_game = streamer["channel"]["game"]
+    streamer_url = streamer["channel"]["url"]
+    streamer_viewers = streamer["viewers"]
+    
+    results.append("%s is playing %s (%s, %s viewer%s)" % (streamer_name, 
+                                                           streamer_game, 
+                                                           streamer_url, 
+                                                           streamer_viewers, 
+                                                           "s" if streamer_viewers != 1 else "" ))
   bot.say(", ".join(results))
 
 
